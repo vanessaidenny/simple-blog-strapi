b6b50fcb706bb5f5d16d9d024dcff69ac3a6d977
Vanessa Isabela Denny <vanessaidenny@hotmail.com>
2024-04-05 20:03:39 -0400

    created nuxt frontend for strapi headless CMS API

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <BlogList :posts="data"></BlogList>
+</template>
+
+<script setup>
+  const API_URL = 'https://my-blogposts-akl8.onrender.com/api/posts?populate[image][fields][0]=url&populate[authors][fields][0]=firstName&populate[authors][fields][1]=lastName&populate[authors][fields][2]=email'
+  const { data } = await useFetch(API_URL)
+
+</script>
---
+++ components/BlogList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="blog-container">
+        <div :for="post in posts.data" :key="post.id">
+            <img class="post-img" :src="post.attributes.image.data.attributes.url" :alt="post.attributes.title">
+            <h2>{{  post.attributes.title }}</h2>
+            <p>{{  post.attributes.summary }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    defineProps({
+        posts: Object
+    })
+</script>
+
+<style>
+    .blog-container {
+        display: flex;
+        gap: 2rem;
+    }
+
+    .post-img {
+        border-radius: 5px;
+    }
+</style>
